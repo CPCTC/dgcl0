b9a35166ab1fd4b46c770b980f925013812326b4
Seth Galasso <seth.galasso@gmail.com>
2021-06-04 14:31:26 -0400

    remove old driver

--- src/driver.lisp
+++ src/driver.lisp
@@ -1,21 +1,3 @@
-#|
-(defpackage driver
-  (:documentation "Public interface for calls to the dgcl0 driver from loaded vehicles.")
-  (:export
-    call	;; Call other functions, but the driver can track your location.
-    sense	;; Gather information from the environment.
-    shoot	;; Fire a projectile that can destroy nodes.
-    rotate      ;; Reorder your four children.
-    re-top      ;; Set the calling-node as the new top level for your entire vehicle.
-    re-parent   ;; Pick a new parent for this node.
-    translate   ;; Shift your vehicle in one of the four cardinal directions.
-    children    ;; Check which children are still attached.
-    release     ;; Split off a child.
-    explode     ;; Destroy the area around the caller.
-    mimic       ;; Take on the appearance of another type of node
-    ))
-|#
-
 ;;;; Containes the public interface for calls to the dgcl0 driver from loaded vehicles.
 ;;;; Non-trivial driver calls should call functions from vehicle-logic.lisp.
 
